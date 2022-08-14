e28241d7ef6dc1f699af0a68ac22e20de6637086
= <ekejubasamuel2@gmail.com>
2022-08-14 16:07:02 +0100

    score board assignment

---
+++ app.js
@@ -0,0 +1,33 @@
+let num1 = document.getElementById('result_screen')
+let num2 = document.getElementById('result_screen_away')
+
+function addOne() {
+  let sum = parseInt(num1.textContent) + 1
+  num1.textContent = sum
+}
+
+function addTwo() {
+  sum = parseInt(num1.textContent) + 2
+  num1.textContent = sum
+}
+
+function addThree() {
+  sum = parseInt(num1.textContent) + 3
+  num1.textContent = sum
+}
+
+// .................................
+function addOne1() {
+  let sum = parseInt(num2.textContent) + 1
+  num2.textContent = sum
+}
+
+function addTwo2() {
+  sum = parseInt(num2.textContent) + 2
+  num2.textContent = sum
+}
+
+function addThree3() {
+  sum = parseInt(num2.textContent) + 3
+  num2.textContent = sum
+}
